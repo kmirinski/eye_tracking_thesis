2215988520e5dd79140775543cfc73294a99bc5c
Konstantin <kosyo32@gmail.com>
2026-06-28 20:19:02 +0200

    poly degrees fix

--- src/config.py
+++ src/config.py
@@ -104,7 +104,7 @@ class KDEConfig:
 
 @dataclass
 class GazeConfig:
-    poly_degrees: list = (1, 2, 3, 4, 5, 6, 8, 12, 15)
+    # poly_degrees: list = (1, 2, 3, 4, 5, 6, 8, 12, 15)
     poly_degrees: list = (2, 3, 4, 5, 6)
     train_ratio: float = 0.8
     val_ratio: float = 0.2
